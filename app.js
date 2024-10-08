e8f8043e87cef068fd6a994fd2de2d05a7478a9b
reneelane0 <w0494485@campus.nscc.ca>
2024-10-08 00:10:00 -0300

    added more pets

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+const imageFolderPath = path.join(__dirname, 'public/images');
+
+// Resize an image to the specified size
+const resizeImage = async (inputPath, outputPath, width, height) => {
+    try {
+        await sharp(inputPath)
+            .resize(width, height)
+            .toFile(outputPath);
+    } catch (error) {
+        console.error('Error resizing image:', error);
+    }
+};
+
+// Resize all images in the folder
+const resizeAllImages = (folderPath, width, height) => {
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.error('Error reading image folder:', err);
+            return;
+        }
+
+        files.forEach((file) => {
+            // Skip files that are already resized
+            if (file.startsWith('resized_')) return;
+        
+            const inputPath = path.join(folderPath, file);
+            const outputPath = path.join(folderPath, `resized_${file}`);
+        
+            resizeImage(inputPath, outputPath, width, height);
+        });
+    });
+};
+
+// Resize all images in the "public/images" folder to 300x300 pixels
+resizeAllImages(imageFolderPath, 300, 300);
 
 // Array of pet names
 const names = [
@@ -13,23 +51,23 @@ const names = [
 // Function to generate random pet data
 const getRandomPet = (id) => {
     const dogBreeds = [
-        { breed: "Golden Retriever", adultImages: ["golden_retriever_adult_1.jpg", "golden_retriever_adult_2.jpg"], puppyImages: ["golden_retriever_puppy_1.jpg", "golden_retriever_puppy_2.jpg"] },
-        { breed: "Labrador", adultImages: ["labrador_adult_1.jpg", "labrador_adult_2.jpg"], puppyImages: ["labrador_puppy_1.jpg", "labrador_puppy_2.jpg"] },
-        { breed: "Bulldog", adultImages: ["bulldog_adult_1.jpg", "bulldog_adult_2.jpg"], puppyImages: ["bulldog_puppy_1.jpg", "bulldog_puppy_2.jpg"] },
-        { breed: "Beagle", adultImages: ["beagle_adult_1.jpg", "beagle_adult_2.jpg"], puppyImages: ["beagle_puppy_1.jpg", "beagle_puppy_2.jpg"] },
-        { breed: "Boxer", adultImages: ["boxer_adult_1.jpg", "boxer_adult_2.jpg"], puppyImages: ["boxer_puppy_1.jpg", "boxer_puppy_2.jpg"] },
-        { breed: "Husky", adultImages: ["husky_adult_1.jpg", "husky_adult_2.jpg"], puppyImages: ["husky_puppy_1.jpg", "husky_puppy_2.jpg"] },
-        { breed: "Pitbull", adultImages: ["pitbull_adult_1.jpg", "pitbull_adult_2.jpg"], puppyImages: ["pitbull_puppy_1.jpg", "pitbull_puppy_2.jpg"] },
-        { breed: "Mixed", adultImages: ["mixed_dog_adult_1.jpg", "mixed_dog_adult_2.jpg"], puppyImages: ["mixed_dog_puppy_1.jpg", "mixed_dog_puppy_2.jpg"] }
+        { breed: "Golden Retriever", adultImages: ["golden_retriever_adult_1.jpg", "golden_retriever_adult_2.jpg", "golden_retriever_adult_3.jpg"], puppyImages: ["golden_retriever_puppy_1.jpg", "golden_retriever_puppy_2.jpg", "golden_retriever_puppy_3.jpg"] },
+        { breed: "Labrador", adultImages: ["labrador_adult_1.jpg", "labrador_adult_2.jpg", "labrador_adult_3.jpg"], puppyImages: ["labrador_puppy_1.jpg", "labrador_puppy_2.jpg", "labrador_puppy_3.jpg"] },
+        { breed: "Bulldog", adultImages: ["bulldog_adult_1.jpg", "bulldog_adult_2.jpg", "bulldog_adult_3.jpg"], puppyImages: ["bulldog_puppy_1.jpg", "bulldog_puppy_2.jpg", "bulldog_puppy_3.jpg"] },
+        { breed: "Beagle", adultImages: ["beagle_adult_1.jpg", "beagle_adult_2.jpg", "beagle_adult_3.jpg"], puppyImages: ["beagle_puppy_1.jpg", "beagle_puppy_2.jpg", "beagle_puppy_3.jpg"] },
+        { breed: "Boxer", adultImages: ["boxer_adult_1.jpg", "boxer_adult_2.jpg", "boxer_adult_3.jpg"], puppyImages: ["boxer_puppy_1.jpg", "boxer_puppy_2.jpg", "boxer_puppy_3.jpg"] },
+        { breed: "Husky", adultImages: ["husky_adult_1.jpg", "husky_adult_2.jpg", "husky_adult_3.jpg"], puppyImages: ["husky_puppy_1.jpg", "husky_puppy_2.jpg", "husky_puppy_3.jpg"] },
+        { breed: "Pitbull", adultImages: ["pitbull_adult_1.jpg", "pitbull_adult_2.jpg", "pitbull_adult_3.jpg"], puppyImages: ["pitbull_puppy_1.jpg", "pitbull_puppy_2.jpg", "pitbull_puppy_3.jpg"] },
+        { breed: "Mixed", adultImages: ["mixed_dog_adult_1.jpg", "mixed_dog_adult_2.jpg", "mixed_dog_adult_3.jpg"], puppyImages: ["mixed_dog_puppy_1.jpg", "mixed_dog_puppy_2.jpg", "mixed_dog_puppy_3.jpg"] }
     ];
     
     const catBreeds = [
-        { breed: "Tabby", adultImages: ["tabby_adult_1.jpg", "tabby_adult_2.jpg"], kittenImages: ["tabby_kitten_1.jpg", "tabby_kitten_2.jpg"] },
-        { breed: "Calico", adultImages: ["calico_adult_1.jpg", "calico_adult_2.jpg"], kittenImages: ["calico_kitten_1.jpg", "calico_kitten_2.jpg"] },
-        { breed: "Ginger", adultImages: ["ginger_adult_1.jpg", "ginger_adult_2.jpg"], kittenImages: ["ginger_kitten_1.jpg", "ginger_kitten_2.jpg"] },
-        { breed: "Siamese", adultImages: ["siamese_adult_1.jpg", "siamese_adult_2.jpg"], kittenImages: ["siamese_kitten_1.jpg", "siamese_kitten_2.jpg"] },
-        { breed: "Persian", adultImages: ["persian_adult_1.jpg", "persian_adult_2.jpg"], kittenImages: ["persian_kitten_1.jpg", "persian_kitten_2.jpg"] },
-        { breed: "Mixed", adultImages: ["mixed_cat_adult_1.jpg", "mixed_cat_adult_2.jpg"], kittenImages: ["mixed_cat_kitten_1.jpg", "mixed_cat_kitten_2.jpg"] }
+        { breed: "Tabby", adultImages: ["tabby_adult_1.jpg", "tabby_adult_2.jpg", "tabby_adult_3.jpg"], kittenImages: ["tabby_kitten_1.jpg", "tabby_kitten_2.jpg", "tabby_kitten_3.jpg"] },
+        { breed: "Calico", adultImages: ["calico_adult_1.jpg", "calico_adult_2.jpg", "calico_adult_3.jpg"], kittenImages: ["calico_kitten_1.jpg", "calico_kitten_2.jpg", "calico_kitten_3.jpg"] },
+        { breed: "Ginger", adultImages: ["ginger_adult_1.jpg", "ginger_adult_2.jpg", "ginger_adult_3.jpg"], kittenImages: ["ginger_kitten_1.jpg", "ginger_kitten_2.jpg", "ginger_kitten_3.jpg"] },
+        { breed: "Siamese", adultImages: ["siamese_adult_1.jpg", "siamese_adult_2.jpg", "siamese_adult_3.jpg"], kittenImages: ["siamese_kitten_1.jpg", "siamese_kitten_2.jpg", "siamese_kitten_3.jpg"] },
+        { breed: "Persian", adultImages: ["persian_adult_1.jpg", "persian_adult_2.jpg", "persian_adult_3.jpg"], kittenImages: ["persian_kitten_1.jpg", "persian_kitten_2.jpg", "persian_kitten_3.jpg"] },
+        { breed: "Mixed", adultImages: ["mixed_cat_adult_1.jpg", "mixed_cat_adult_2.jpg", "mixed_cat_adult_3.jpg"], kittenImages: ["mixed_cat_kitten_1.jpg", "mixed_cat_kitten_2.jpg", "mixed_cat_kitten_3.jpg"] }
     ];
 
     const cities = ["Halifax", "Fredericton", "Charlottetown", "St. John", "Moncton"];
